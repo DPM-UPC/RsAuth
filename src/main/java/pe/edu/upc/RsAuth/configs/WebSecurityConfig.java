83e6fdffeceb2bf972bf836d827439834b8d61aa
paolo ortega <paolosp29@gmail.com>
2018-07-15 13:16:57 -0500

    ajuste para permitir libre registro

--- src/main/java/pe/edu/upc/RsAuth/configs/WebSecurityConfig.java
+++ src/main/java/pe/edu/upc/RsAuth/configs/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.RsAuth.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers("/accessTokens", "/v2/**", "/api/**", "/swagger-ui.html/**", "/webjars/**", "/swagger-resources/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
